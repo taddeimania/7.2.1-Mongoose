30833bf07afe0206b4eb10298ee3f9952201b365
taddeimania <jtaddei@gmail.com>
2017-07-06 11:50:58 -0400

    How does I update?

--- app.js
+++ app.js
@@ -5,11 +5,8 @@ mongoose.Promise = require("bluebird");
 
 mongoose.connect("mongodb://localhost:27017/mongoosestore");
 
-const recipe = new Recipe({name: "Joel Salad"});
-recipe.save();
-
-const ingredient = {amount: 5, name: "Tomatoes"}
-recipe.ingredients.push(ingredient);
-recipe.save();
-
-console.log(recipe.toObject());
+Recipe.findOne({name: "Joel Salad"}).then(result => {
+  const ingredient = {name: "Bacon Fat", amount: 4}
+  result.ingredients.push(ingredient);
+  result.save();
+})
