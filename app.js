2d7a366c3220b335b6c5c73cfab16da731c331ae
taddeimania <jtaddei@gmail.com>
2017-07-06 10:26:17 -0400

    Creating models

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const mongoose = require("mongoose");
+const Recipe = require("./models/recipe");
+
 mongoose.Promise = require("bluebird");
 
 mongoose.connect("mongodb://localhost:27017/mongoosestore");
+
+const recipe = new Recipe({name: "Joel Salad"});
+recipe.save();
+
+const ingredient = {amount: 5, name: "Tomatoes"}
+recipe.ingredients.push(ingredient);
+recipe.save();
+
+console.log(recipe.toObject());
--- models/recipe.js
+++ models/recipe.js
@@ -10,3 +10,7 @@ const recipeSchema = new mongoose.Schema({
   }],
   instructions: [String]
 });
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe;
